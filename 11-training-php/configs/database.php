177b5a1f449478b373f117ddb9ed5af3ff61c1b0
wedbanhang <capham18@gmail.com>
2025-09-19 09:34:23 +0700

    php

---
+++ 11-training-php/configs/database.php
@@ -0,0 +1,11 @@
+<?php
+define('DB_HOST', 'mysql');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'rootpass');  // Cập nhật mật khẩu
+define('DB_PORT', 3306);
+define('DB_NAME', 'app_web1');
+// // Kết nối database
+// $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
